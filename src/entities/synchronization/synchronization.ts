ba451a47b29e0ad9ac492e678a53b53d970670f5
Ruben van der Linde <ruben@conduction.nl>
2024-10-01 22:26:01 +0200

    fixing the ts objects

--- src/entities/synchronization/synchronization.ts
+++ src/entities/synchronization/synchronization.ts
@@ -11,19 +11,11 @@ export class Synchronization implements TSynchronization {
 	public sourceHash: string
 	public sourceTargetMapping: string
 	public sourceConfig: object
-	public sourceLastChanged: string
-	public sourceLastChecked: string
-	public sourceLastSynced: string
 	public targetId: string
 	public targetType: string
 	public targetHash: string
 	public targetSourceMapping: string
 	public targetConfig: object
-	public targetLastChanged: string
-	public targetLastChecked: string
-	public targetLastSynced: string
-	public created: string
-	public updated: string
 
 	constructor(synchronization: TSynchronization) {
 		this.id = synchronization.id || ''
@@ -34,19 +26,11 @@ export class Synchronization implements TSynchronization {
 		this.sourceHash = synchronization.sourceHash || null
 		this.sourceTargetMapping = synchronization.sourceTargetMapping || null
 		this.sourceConfig = synchronization.sourceConfig || null
-		this.sourceLastChanged = synchronization.sourceLastChanged || null
-		this.sourceLastChecked = synchronization.sourceLastChecked || null
-		this.sourceLastSynced = synchronization.sourceLastSynced || null
 		this.targetId = synchronization.targetId || null
 		this.targetType = synchronization.targetType || null
 		this.targetHash = synchronization.targetHash || null
 		this.targetSourceMapping = synchronization.targetSourceMapping || null
 		this.targetConfig = synchronization.targetConfig || null
-		this.targetLastChanged = synchronization.targetLastChanged || null
-		this.targetLastChecked = synchronization.targetLastChecked || null
-		this.targetLastSynced = synchronization.targetLastSynced || null
-		this.created = synchronization.created || null
-		this.updated = synchronization.updated || null
 
 	}
 
