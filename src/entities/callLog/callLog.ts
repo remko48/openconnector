8c5b2552fb6859e894737701342bd2ec1837c512
Thijn <thijn@conduction.nl>
2024-10-18 11:33:47 +0200

    fixed call log schema

--- src/entities/callLog/callLog.ts
+++ src/entities/callLog/callLog.ts
@@ -1,39 +1,40 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { SafeParseReturnType, z } from 'zod'
 import { TCallLog } from './callLog.types'
+import getValidISOstring from '../../services/getValidISOstring.js'
 import ReadonlyBaseClass from '../ReadonlyBaseClass.js'
 
 export class CallLog extends ReadonlyBaseClass implements TCallLog {
 
-	public readonly id?: string
+	public readonly id: number
 	public readonly sourceId: string
 	public readonly endpoint: string
 	public readonly method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'
 	public readonly statusCode: number
-	public readonly requestHeaders?: object
-	public readonly requestBody?: any
-	public readonly responseHeaders?: object
-	public readonly responseBody?: any
+	public readonly requestHeaders: object
+	public readonly requestBody: any
+	public readonly responseHeaders: object
+	public readonly responseBody: any
 	public readonly duration: number
-	public readonly error?: string | null
+	public readonly error: string | null
 	public readonly created: string
-	public readonly updated?: string | null
+	public readonly updated: string | null
 
 	constructor(callLog: TCallLog) {
 		const processedCallLog = {
-			id: callLog.id,
-			sourceId: callLog.sourceId,
-			endpoint: callLog.endpoint,
-			method: callLog.method,
-			statusCode: callLog.statusCode,
-			requestHeaders: callLog.requestHeaders,
-			requestBody: callLog.requestBody,
-			responseHeaders: callLog.responseHeaders,
-			responseBody: callLog.responseBody,
-			duration: callLog.duration,
-			error: callLog.error || null,
-			created: callLog.created,
-			updated: callLog.updated || null,
+			id: callLog.id || '',
+			sourceId: callLog.sourceId || '',
+			endpoint: callLog.endpoint || '',
+			method: callLog.method || 'GET',
+			statusCode: callLog.statusCode || 0,
+			requestHeaders: callLog.requestHeaders || {},
+			requestBody: callLog.requestBody || {},
+			responseHeaders: callLog.responseHeaders || {},
+			responseBody: callLog.responseBody || {},
+			duration: callLog.duration || 0,
+			error: callLog.error || '',
+			created: getValidISOstring(callLog.created) ?? '',
+			updated: getValidISOstring(callLog.updated) ?? '',
 		}
 
 		super(processedCallLog)
@@ -41,19 +42,19 @@ export class CallLog extends ReadonlyBaseClass implements TCallLog {
 
 	public validate(): SafeParseReturnType<TCallLog, unknown> {
 		const schema = z.object({
-			id: z.string().uuid().optional(),
-			sourceId: z.string().uuid(),
+			id: z.number().nullable(),
+			sourceId: z.string(),
 			endpoint: z.string(),
 			method: z.enum(['GET', 'POST', 'PUT', 'DELETE', 'PATCH']),
 			statusCode: z.number().int().positive(),
-			requestHeaders: z.record(z.any()).optional(),
-			requestBody: z.any().optional(),
-			responseHeaders: z.record(z.any()).optional(),
-			responseBody: z.any().optional(),
+			requestHeaders: z.record(z.any()),
+			requestBody: z.any(),
+			responseHeaders: z.record(z.any()),
+			responseBody: z.any(),
 			duration: z.number().positive(),
-			error: z.string().nullable().optional(),
-			created: z.string().datetime(),
-			updated: z.string().datetime().nullable().optional(),
+			error: z.string(),
+			created: z.string().datetime().or(z.literal('')),
+			updated: z.string().datetime().or(z.literal('')),
 		})
 
 		return schema.safeParse({ ...this })
