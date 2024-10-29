0ebda60368b10d29823654ec4908fd857b04ba77
Thijn <Thijn@conduction.nl>
2024-10-29 16:08:50 +0100

    applied requested changes

--- src/modals/MappingTest/components/TestMappingResult.vue
+++ src/modals/MappingTest/components/TestMappingResult.vue
@@ -13,11 +13,11 @@
 				<p>{{ mappingTest.error }}</p>
 			</NcNoteCard>
 
-			<NcGuestContent class="result">
+			<div class="result">
 				<pre><!-- do NOT remove this comment
 					-->{{ JSON.stringify(mappingTest.result, null, 2) }}
 				</pre>
-			</NcGuestContent>
+			</div>
 		</div>
 	</div>
 </template>
@@ -53,6 +53,16 @@ export default {
     text-align: left;
 }
 .result {
-	margin-inline: 0.5rem !important;
+    color: var(--color-main-text);
+    background-color: var(--color-main-background);
+    min-width: 0;
+    border-radius: var(--border-radius-large);
+    box-shadow: 0 0 10px var(--color-box-shadow);
+    height: fit-content;
+    padding: 15px;
+    margin: 20px 0.5rem;
+}
+.result pre {
+	white-space: break-spaces;
 }
 </style>
