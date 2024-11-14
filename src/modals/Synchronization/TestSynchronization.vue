4ff6d4528652cf6acf090ac5628b2dd7c10c6670
Remko <remko@conduction.nl>
2024-11-14 15:22:57 +0100

    cleanup

--- src/modals/Synchronization/TestSynchronization.vue
+++ src/modals/Synchronization/TestSynchronization.vue
@@ -14,30 +14,30 @@ import { synchronizationStore, navigationStore } from '../../store/store.js'
 				<NcLoadingIcon :size="64" name="Running synchronization test" />
 			</div>
 
-			<NcNoteCard v-if="response && response.status.toString().startsWith('2')" type="success">
+			<NcNoteCard v-if="response?.ok" type="success">
 				<p>The connection to the synchronization was successful.</p>
 			</NcNoteCard>
-			<NcNoteCard v-if="(response && !response.status.toString().startsWith('2')) || error" type="error">
+			<NcNoteCard v-if="!response?.ok || error" type="error">
 				<p>
 					An error occurred while testing the connection: {{
 						synchronizationStore.synchronizationTest
 							? synchronizationStore.synchronizationTest.message
 								? synchronizationStore.synchronizationTest.message
 								: synchronizationStore.synchronizationTest.error
-							: response.statusMessage
-								? response.statusMessage
-								: `${response.status} - ${response.statusText}`
+							: response?.statusMessage
+								? response?.statusMessage
+								: `${response?.status} - ${response?.statusText}`
 					}}
 				</p>
 			</NcNoteCard>
 
 			<div v-if="response">
-				<p><b>Status:</b> {{ response.statusText }} ({{ response.status }})</p>
-				<p><b>Response time:</b> {{ response.responseTime ?? 'Onbekend' }} (Milliseconds)</p>
-				<p><b>Size:</b> {{ response.size ?? 'Onbekend' }} (Bytes)</p>
-				<p><b>Remote IP:</b> {{ response.remoteIp }}</p>
-				<p><b>Headers:</b> {{ response.headers }}</p>
-				<p><b>Body:</b> {{ response.body }}</p>
+				<p><b>Status:</b> {{ response?.statusText }} ({{ response?.status }})</p>
+				<p><b>Response time:</b> {{ response?.responseTime ?? 'Onbekend' }} (Milliseconds)</p>
+				<p><b>Size:</b> {{ response?.size ?? 'Onbekend' }} (Bytes)</p>
+				<p><b>Remote IP:</b> {{ response?.remoteIp }}</p>
+				<p><b>Headers:</b> {{ response?.headers }}</p>
+				<p><b>Body:</b> {{ response?.body }}</p>
 			</div>
 		</div>
 	</NcModal>
