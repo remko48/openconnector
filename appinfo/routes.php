a2f33a8be1ecaae6f01acea1eedbfa2dcc7849d1
Wilco Louwerse <wilco@conduction.nl>
2025-01-13 17:08:41 +0100

    Fix routes

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -18,22 +18,40 @@ return [
 		['name' => 'jobs#logs', 'url' => '/api/jobs-logs/{id}', 'verb' => 'GET'],
 		['name' => 'endpoints#test', 'url' => '/api/endpoints-test/{id}', 'verb' => 'POST'],
 		['name' => 'endpoints#logs', 'url' => '/api/endpoints-logs/{id}', 'verb' => 'GET'],
+
+		// Synchronization endpoints
 		['name' => 'synchronizations#contracts', 'url' => '/api/synchronizations-contracts/{id}', 'verb' => 'GET'],
 		['name' => 'synchronizations#logs', 'url' => '/api/synchronizations-logs/{id}', 'verb' => 'GET'],
 		['name' => 'synchronizations#test', 'url' => '/api/synchronizations-test/{id}', 'verb' => 'POST'],
 		['name' => 'synchronizations#run', 'url' => '/api/synchronizations-run/{id}', 'verb' => 'POST'],
+
 		// Mapping endpoints
 		['name' => 'mappings#test', 'url' => '/api/mappings/test', 'verb' => 'POST'],
 		['name' => 'mappings#saveObject', 'url' => '/api/mappings/objects', 'verb' => 'POST'],
 		['name' => 'mappings#getObjects', 'url' => '/api/mappings/objects', 'verb' => 'GET'],
+
 		// Running endpoints - allow any path after /api/endpoints/
 		['name' => 'endpoints#handlePath', 'postfix' => 'read', 'url' => '/api/endpoint/{_path}', 'verb' => 'GET', 'requirements' => ['_path' => '.+']],
 		['name' => 'endpoints#handlePath', 'postfix' => 'update', 'url' => '/api/endpoint/{_path}', 'verb' => 'PUT', 'requirements' => ['_path' => '.+']],
 		['name' => 'endpoints#handlePath', 'postfix' => 'create', 'url' => '/api/endpoint/{_path}', 'verb' => 'POST', 'requirements' => ['_path' => '.+']],
 		['name' => 'endpoints#handlePath', 'postfix' => 'destroy', 'url' => '/api/endpoint/{_path}', 'verb' => 'DELETE', 'requirements' => ['_path' => '.+']],
+
 		// Import & Export
 		['name' => 'import#import', 'url' => '/api/import', 'verb' => 'POST'],
 //		['name' => 'import#importUpdate', 'url' => '/api/import/{id}', 'verb' => 'PUT'],
 		['name' => 'export#export', 'url' => '/api/export/{type}/{id}', 'verb' => 'GET'],
+
+		// Event messages
+		['name' => 'event#messages', 'url' => '/api/events/{id}/messages', 'verb' => 'GET'],
+
+		// Subscription management
+		['name' => 'event#subscribe', 'url' => '/api/events/subscriptions', 'verb' => 'POST'],
+		['name' => 'event#updateSubscription', 'url' => '/api/events/subscriptions/{subscriptionId}', 'verb' => 'PUT'],
+		['name' => 'event#unsubscribe', 'url' => '/api/events/subscriptions/{subscriptionId}', 'verb' => 'DELETE'],
+		['name' => 'event#subscriptions', 'url' => '/api/events/subscriptions', 'verb' => 'GET'],
+		['name' => 'event#subscriptionMessages', 'url' => '/api/events/subscriptions/{subscriptionId}/messages', 'verb' => 'GET'],
+
+		// Pull-based delivery
+		['name' => 'event#pull', 'url' => '/api/events/subscriptions/{subscriptionId}/pull', 'verb' => 'GET'],
 	],
 ];
