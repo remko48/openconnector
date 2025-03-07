26cfebc2d6ff7185a6e3b30ab920cb427fb867c5
Ruben van der Linde <rubenvdlinde@gmail.com>
2025-03-07 07:42:27 +0100

    Website name fix

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -65,7 +65,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'OpenConnector',
+        title: 'Open Connector',
         logo: {
           alt: 'OpenConnector Logo',
           src: 'img/logo.svg',
